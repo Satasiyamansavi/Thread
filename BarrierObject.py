1632a8ac879a368b1cd9424592c2c0a67a2c2a24
Mansvi <mansvi@gmail.com>
2026-02-18 18:48:45 +0530

    Comleted code

--- BarrierObject.py
+++ BarrierObject.py
@@ -1,14 +1,15 @@
 import threading 
 import time
+barrier=threading.Barrier(3)
 def player(name):
     print(f"{name} is started")
     score=0
     for i in range(3):
         time.sleep(1)
         print(f"{name} is palying")
-
+    barrier.wait()  
     print(f"{name} scored {score}")
-    print("Sending winning amount to {name}")
+    print(f"Sending winning amount to {name}")
 Thread=[]
 player_name=['Raj','Rohan','Priya']
 for name in player_name:
