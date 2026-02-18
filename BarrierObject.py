998162167b3c4b8a8e26a1a9a07dda5a096b950f
Mansvi <mansvi@gmail.com>
2026-02-18 18:41:03 +0530

    Barrier object more code add

--- BarrierObject.py
+++ BarrierObject.py
@@ -8,4 +8,10 @@ def player(name):
         print(f"{name} is palying")
 
     print(f"{name} scored {score}")
-    print()
+    print("Sending winning amount to {name}")
+Thread=[]
+player_name=['Raj','Rohan','Priya']
+for name in player_name:
+    thread=threading.Thread(target=player,args=(name,))
+    Thread.append(thread)
+    thread.start()
